ce0926c9f8bdb043223ef0d93da6ded82a62ef9d
msihabudin <seehabb@gmail.com>
2021-11-27 13:43:20 +0700

    clean up

--- app/src/main/java/com/sihabudin/vidiotest/adapter/EpisodeViewHolder.kt
+++ app/src/main/java/com/sihabudin/vidiotest/adapter/EpisodeViewHolder.kt
@@ -1,35 +1,27 @@
 package com.sihabudin.vidiotest.adapter
 
-import android.util.Log
 import androidx.core.view.isVisible
-import androidx.lifecycle.findViewTreeLifecycleOwner
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.bumptech.glide.Glide
 import com.sihabudin.vidiotest.R
 import com.sihabudin.vidiotest.databinding.ItemEpisodeBinding
 import com.sihabudin.vidiotest.enum.DownloadStatus
 import com.sihabudin.vidiotest.model.Episode
-import com.sihabudin.vidiotest.workmanager.DownloadWorker
-import java.util.Observer
 
 class EpisodeViewHolder(
     val binding: ItemEpisodeBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(episode: Episode,  onDownloadClick: (Episode) -> Unit) {
+    fun bind(episode: Episode, onDownloadClick: (Episode) -> Unit) {
         binding.titleView.text = episode.title
         binding.descriptionView.text = episode.description
         binding.durationView.text = episode.duration
-        binding.progressView.isVisible =(episode.downloadStatus == DownloadStatus.DOWNLOADING)
-        binding.downloadButton.isVisible = (episode.downloadStatus !== DownloadStatus.DOWNLOAD_FINISH )
+        binding.progressView.isVisible = (episode.downloadStatus == DownloadStatus.DOWNLOADING)
+        binding.downloadButton.isVisible =
+            (episode.downloadStatus !== DownloadStatus.DOWNLOAD_FINISH)
         binding.downloadButton.setOnClickListener {
             onDownloadClick(episode)
-            binding.progressView.isVisible =(episode.downloadStatus == DownloadStatus.DOWNLOADING)
+            binding.progressView.isVisible = (episode.downloadStatus == DownloadStatus.DOWNLOADING)
 
             /***
              * TODO :
